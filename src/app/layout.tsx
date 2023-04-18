0b022eb133ec5ce519a56593b448fe73ec491af9
William <euwilliamsantana@gmail.com>
2023-04-18 11:57:26 -0300

    refactor: Font-family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,16 @@ import { CardContextProvider } from './context/CardContext'
 import './globals.css'
 import { Source_Sans_Pro } from 'next/font/google'
 
-const sourceSansPro = Source_Sans_Pro({ subsets: ['latin'], weight: ["400", "600", "700"]})
+const sourceSansPro = Source_Sans_Pro({
+  subsets:['latin'],
+  weight: ['400', '600', '700']
+})
+
+
+export const metadata = {
+  title: "Cartão de Crédito",
+  description: "Card data"
+}
 
 export default function RootLayout({
   children,
